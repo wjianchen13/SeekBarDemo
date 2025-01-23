9abdeac95a1773e4640e48e428401fb374b5fbc5
cwj <282330332@qq.com>
2025-01-23 11:38:54 +0800

    水平seekbar，进度条渐变

---
+++ app/src/main/java/com/example/seekbardemo/Utils.java
@@ -0,0 +1,8 @@
+package com.example.seekbardemo;
+
+public class Utils {
+
+    public static void log(String str) {
+        System.out.println("==========================> " + str);
+    }
+}
--- app/src/main/java/com/example/seekbardemo/test1/TestActivity1.java
+++ app/src/main/java/com/example/seekbardemo/test1/TestActivity1.java
@@ -1,12 +1,17 @@
 package com.example.seekbardemo.test1;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.View;
+import android.widget.SeekBar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.seekbardemo.R;
-import com.example.seekbardemo.vertical_seekbar2.VerticalColorSeekBar;
+import com.example.seekbardemo.Utils;
 
 /**
  * https://blog.csdn.net/Agg_bin/article/details/131676712
@@ -15,38 +20,60 @@ import com.example.seekbardemo.vertical_seekbar2.VerticalColorSeekBar;
  */
 public class TestActivity1 extends AppCompatActivity{
 
-    private VerticalColorSeekBar seekBar;
+    private SeekBar seekBar;
+    private int mProgress;
+    private Handler mHandler = new Handler(Looper.myLooper()) {
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            if(seekBar != null) {
+                mProgress += 5;
+                seekBar.setProgress(mProgress);
+            }
+            sendEmptyMessageDelayed(1, 2000);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test1);
-//        seekBar = findViewById(R.id.seek_gesture_duration);
+        seekBar = findViewById(R.id.sb_normal);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Utils.log("onProgressChanged progress: " + progress + "  fromUser: " + fromUser);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Utils.log("onStartTrackingTouch");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Utils.log("onStopTrackingTouch");
+            }
+        });
     }
 
     /**
-     * 初始化 Seekbar
+     * Seekbar 设置进度
      * @param v
      */
     public void onTest1(View v) {
-//        seekBar.setMax(9000);
-//        seekBar.setProgress(3600);
-//        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-//            @Override
-//            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-////                floatView.tvGestureDuration.setText(String.valueOf(i));
-//            }
-//
-//            @Override
-//            public void onStartTrackingTouch(SeekBar seekBar) {
-//
-//            }
-//
-//            @Override
-//            public void onStopTrackingTouch(SeekBar seekBar) {
-//
-//            }
-//        });
+        seekBar.setProgress(80);
+
+    }
+
+
+    /**
+     * Seekbar 设置进度
+     * @param v
+     */
+    public void onTest2(View v) {
+        mHandler.sendEmptyMessageDelayed(1, 2000);
 
     }
 
